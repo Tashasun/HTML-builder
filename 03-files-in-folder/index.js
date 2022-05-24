78a8b9c22419650136cbab9862fad851c758098b
Tashasun <natalia.limbakh@gmail.com>
2022-05-24 16:11:29 +0300

    fix: fix task 1-4 with eslint

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,25 +3,25 @@ const path = require('path');
 
 
 fs.readdir(path.resolve(__dirname, 'secret-folder'), { withFileTypes: true }, (err, files) => {
-    if (err)
-        console.log(err);
-    else {
-        files.forEach(file => {
-            let result = ""
-            if (file.isFile()) {
-                const result = file.name.split('.')[0] + ' - '+path.extname(file.name).split('.')[1];
+  if (err)
+    console.log(err);
+  else {
+    files.forEach(file => {
+     
+      if (file.isFile()) {
+        const result = file.name.split('.')[0] + ' - '+path.extname(file.name).split('.')[1];
 
-                const fileName = path.resolve(__dirname, 'secret-folder', file.name)
+        const fileName = path.resolve(__dirname, 'secret-folder', file.name);
 
-                fs.stat(fileName, (error, stats) => {
-                    if (error) { console.log(error) }
-                    else {
-                        console.log(result+' - '+stats.size)
-                    }
-                })
-            }
-        })
-    }
+        fs.stat(fileName, (error, stats) => {
+          if (error) { console.log(error); }
+          else {
+            console.log(result+' - '+stats.size);
+          }
+        });
+      }
+    });
+  }
 
-})
+});
 
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,38 +2,38 @@ const fs = require('fs');
 const path = require('path');
 
 new Promise((resolve, reject) => {
-    fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true }, err => {
-        if (err) reject(err);
-        console.log('create dir ')
-        resolve();
-    })
+  fs.mkdir(path.resolve(__dirname, 'files-copy'), { recursive: true }, err => {
+    if (err) reject(err);
+    console.log('create dir ');
+    resolve();
+  });
 }).then(
-    () => {
-        return new Promise((resolve, reject) => {
-            fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
-                if (err) reject(err);
-                resolve(files);
-            });
-        })
-    }
+  () => {
+    return new Promise((resolve, reject) => {
+      fs.readdir(path.resolve(__dirname, 'files'), (err, files) => {
+        if (err) reject(err);
+        resolve(files);
+      });
+    });
+  }
 ).then(
-    files => {
-        return Promise.all(
-            files.map(f=>{
-                return new Promise((r)=>{
-                    fs.createReadStream(path.resolve(__dirname, 'files', f))
-                   .pipe(fs.createWriteStream(path.resolve(__dirname, 'files-copy', f)));
-                   r(f);
-                })
-            })
-        )
-    }
+  files => {
+    return Promise.all(
+      files.map(f=>{
+        return new Promise((r)=>{
+          fs.createReadStream(path.resolve(__dirname, 'files', f))
+            .pipe(fs.createWriteStream(path.resolve(__dirname, 'files-copy', f)));
+          r(f);
+        });
+      })
+    );
+  }
 ).then(
-    arr => {
-        console.log(`coped ${arr.length} files`)
+  arr => {
+    console.log(`coped ${arr.length} files`);
         
-    }
-).catch(err => console.log(err))
+  }
+).catch(err => console.log(err));
 
 
 
